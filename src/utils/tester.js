6f1a23ae53d9dcb479a94cb41e8b0e10e78ecc95
timothydowd <timdowd1984@gmail.com>
2019-08-17 14:25:01 +0100

    rewritten formatJsondata to create better shape of json

--- src/utils/tester.js
+++ src/utils/tester.js
@@ -1,6 +1,6 @@
-const { extractRootWords, writeEmptyWordDataJson } = require('./utils')
-const wordData = require('../data/word-data.json')
+const { extractRootWords, writeEmptyWordDataJson, formatJsonData } = require('./utils')
+// const wordData = require('../data/word-data.json')
 const rootWords = require('../data/rootWords.json')
 
 // extractRootWords(wordData)
-writeEmptyWordDataJson(rootWords)
+formatJsonData(rootWords)
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,24 +5,35 @@ const fs = require('fs')
 
 
 const formatJsonData = (json) => {
-    const formattedData = json.reduce((reducedData, object) => {
-        const word = object["Word"].trim()
-        const synonyms = [object["Synonym 1"], object["Synonym 2"], object["Synonym 3"], object["Synonym 4"]]
-        const antonyms = [object["Antonym 1"], object["Antonym 2"], object["Antonym 3"], object["Antonym 4"]]
+    const formattedData = json.map((word) => {
+        const trimmedWord = word.trim()
+        const synonyms = []
+        const antonyms = []
 
-        const removedBlankSynonyms = synonyms.reduce((reducedSynonyms, synonym) => {
-            return synonym === "" ? [...reducedSynonyms] : [ ...reducedSynonyms, synonym]    
-        },[])
+        // const removedBlankSynonyms = synonyms.reduce((reducedSynonyms, synonym) => {
+        //     return synonym === "" ? [...reducedSynonyms] : [ ...reducedSynonyms, synonym]    
+        // },[])
 
-        const removedBlankAntonyms = antonyms.reduce((reducedAntonyms, antonym) => {
-            return antonym === "" ? [...reducedAntonyms] : [ ...reducedAntonyms, antonym]    
-        },[])
+        // const removedBlankAntonyms = antonyms.reduce((reducedAntonyms, antonym) => {
+        //     return antonym === "" ? [...reducedAntonyms] : [ ...reducedAntonyms, antonym]    
+        // },[])
 
-        return [ ...reducedData, { word, synonyms: removedBlankSynonyms, antonyms: removedBlankAntonyms}]
-    },[])
 
-    
-    return formattedData
+
+        // return [ ...reducedData, { word, synonyms: removedBlankSynonyms, antonyms: removedBlankAntonyms}]
+        return [{ word: trimmedWord, synonyms, antonyms, checked: false}]
+    })
+
+    fs.writeFile("./emptyWordData.json", JSON.stringify(formattedData, null, 2), err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        //file written successfully
+        });
+
+
+    // return formattedData
 }
 
 
